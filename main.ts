8902b4c8c36a1baeb86e7a7908f22ae2c295e749
DemperDemo <114505894+DemperDemo@users.noreply.github.com>
2022-09-27 17:38:51 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+let item = 0
+input.onGesture(Gesture.Shake, function () {
+    item = randint(0, 2)
+    if (item == 0) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+    if (item == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (item == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+})
